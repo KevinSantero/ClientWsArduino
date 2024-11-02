8f8f711ccb650ec3835165ed12ba5f0cdb09964e
KevinSantero <37110286+KevinSantero@users.noreply.github.com>
2024-11-01 21:43:09 -0500

    .

---
+++ ClienteWsArduino/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cliente ws!");
+
+string name;
+
+while (true)
+{
+	Console.WriteLine("Digite su nombre");
+	name = Console.ReadLine();
+	break;
+}
+
+var ws = new ClientWebSocket();
+
+//await ws.ConnectAsync(new Uri("ws://localhost:6969/ws"),
+//  CancellationToken.None);
+
+await ws.ConnectAsync(new Uri($"ws://localhost:6969/ws?name={name}"),
+  CancellationToken.None);
+
+Console.WriteLine("Conectado al servidor!");
+
+var sendTask = Task.Run(async () =>
+{
+	while (true)
+	{
+		string message  = Console.ReadLine();
+
+		if (message == "exit")
+			break;
+
+        var bytes =  Encoding.UTF8.GetBytes(message);
+
+		await ws.SendAsync(new ArraySegment<byte>(bytes),WebSocketMessageType.Text,true, CancellationToken.None);
+
+	}
+});
+
+//await sendTask;
+
+var receiveTask = Task.Run(async () =>
+{
+	var buffer = new byte[1024];
+	while (true)
+	{
+		var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+		if (result.MessageType == WebSocketMessageType.Close)
+		{
+			break;
+		}
+		var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+		Console.WriteLine("Received: " + message);
+	}
+});
+
+
+//await receiveTask;
+
+
+await Task.WhenAll(receiveTask,sendTask);
+if (ws.State == WebSocketState.Closed) {
+
+	await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+}
+
+await Task.WhenAll(receiveTask,sendTask);
